3a6997f80b3e8cc3f3c15b54fa52f06f16629af1
root <joshiantozer@gmail.com>
2021-10-26 19:44:53 +0000

    completed assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,87 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  temp = arr.max(2)
+  temp.sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length > 1 and arr.combination(2).find{|x,y| x+y==n}
+    return true
+  else
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return 'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.is_a? String and s.length > 0
+    return (s =~ /[aeiouAEIOU]/) != 0
+  else
+    return false
+  end
 end
 
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.to_i !~ /[\b[01]+\b]/
+    n = s.to_i(2)
+    if n % 4 == 0
+      true
+    else
+      false
+    end
+  elsif s.length == 0
+    print('The empty string is not a valid binary number!')
+  else
+    print(s + ' is not a valid binary number!')
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+
+  raise ArgumentError.new(
+    "ISBN cannot be an empty string."
+  ) if isbn == ''
+
+  raise ArgumentError.new(
+    "Price cannot be less than or equal to 0."
+  ) if price <= 0
+
+  @isbn = isbn
+  @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_as_string
+    '$%.2f' % price
+  end
+
 end
